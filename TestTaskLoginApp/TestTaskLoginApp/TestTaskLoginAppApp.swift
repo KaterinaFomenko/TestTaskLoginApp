4d2d89df5ccd2aa12cebc68b7d40389a6e37a2ae
KaterinaFomenko <katerinafomenko77@gmail.com>
2025-06-17 14:16:15 +0200

    Add CoreData

--- TestTaskLoginApp/TestTaskLoginApp/TestTaskLoginAppApp.swift
+++ TestTaskLoginApp/TestTaskLoginApp/TestTaskLoginAppApp.swift
@@ -6,13 +6,25 @@
 //
 
 import SwiftUI
+import CoreData
 
 @main
 struct TestTaskLoginAppApp: App {
+    let persistentContainer = NSPersistentContainer(name: "CoreModel")
     var body: some Scene {
         WindowGroup {
            // ContentView()
             UsersTabView()
+                .environment(\.managedObjectContext, persistentContainer.viewContext)
         }
     }
+    
+    init() {
+            persistentContainer.loadPersistentStores { description, error in
+                if let error = error {
+                    fatalError("Core Data failed to load: \(error.localizedDescription)")
+                }
+            }
+        }
+    
 }
